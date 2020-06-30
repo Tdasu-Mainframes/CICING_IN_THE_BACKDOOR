c551dd139baf04ab97aee3ea6cb66139e01c176d
jake <jake.labelle@hotmail.co.uk>
2020-06-30 22:07:24 +0100

    ta daaa, we done here

--- convert_jcl.py
+++ convert_jcl.py
@@ -6,10 +6,16 @@ count = 1
 for line in lines:
     line = line.replace("\n","")
     line = line.replace("'","''")
-    half = int(len(line)/2)
-    line1 = line[:half]
-    line2 = line[half:]
-    output_cob.write("     MOVE '" + line1 + "\n")
-    output_cob.write("-    " + line2 + "'\n")
-    output_cob.write("-    TO WS-LINE("+ str(count)+")\n")
+    if len(line) == 0:
+        line = " "
+    if len(line) <= 38:
+        output_cob.write("     MOVE '" + line + "' TO WS-LINE("+ str(count)+")\n")
+    elif len(line) <= 54:
+        output_cob.write("     MOVE '" + line + "'\n")
+        output_cob.write("-     TO WS-LINE("+ str(count)+")\n")
+    else:
+        line1 = line[:55]
+        line2 = line[55:]
+        output_cob.write("     MOVE '" + line1 + "\n")
+        output_cob.write("-    '" + line2 + "' TO WS-LINE("+ str(count)+")\n")
     count += 1
